d497883bc649a05d4b62ab21a50b7fc2f92ee65f
RubyOn <rubyon@me.com>
2019-06-19 15:53:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# homebridge-commax-switch
+
+Commax 월패드의 Homebridge plugin 입니다.
+아래의 조건이 만족하여야 사용이 가능 합니다.
+- Commax 의 월패드가 Ruvie 사이트를 지원하여야 한다.
+- Ruvie 사이트에 접속하여 웹페이지 소스보기를 하여 Commax 서버의 ip 와 우리집의 ip 를 알아낼수 있어야 한다. (차후 이 페이지를 통해 설명함)
+
+### 컨트롤 가능한 디바이스
+- Commax 월패드로 On/Off 가능한 전등 스위치 (light)
+- Commax 월패드로 off 가능한 가스밸브 스위치 (gas)
+
+### Installation
+
+```sh
+$ sudo npm install -g --unsafe-perm homebridge-commax-switch
+```
+
+### Homebridge Config.json
+
+```json
+"accessories": [
+        {
+            "accessory": "CommaxSwitch",
+            "name": "전등 1",
+            "commax_ip": "14.x.x.x",
+            "home_ip": "10.x.x.x",
+            "device": "light",
+            "device_id": 1,
+            "interval": 5000
+        },
+        {
+            "accessory": "CommaxSwitch",
+            "name": "전등 2",
+            "commax_ip": "14.x.x.x",
+            "home_ip": "10.x.x.x",
+            "device": "light",
+            "device_id": 2,
+            "interval": 5000
+        },
+        {
+            "accessory": "CommaxSwitch",
+            "name": "가스밸브",
+            "commax_ip": "14.x.x.x",
+            "home_ip": "10.x.x.x",
+            "device": "gas",
+            "device_id": 3,
+            "interval": 5000
+        }
+    ]
+```
+| Config | Value | Required | Default |
+| ------ | ----- | ----- | ----- |
+| name | 스위치에 사용할 이름 | true | |
+| commax_ip | Commax 서버 ip | true | |
+| home_ip | Commax 에 등록된 우리집 ip | true | |
+| device | 스위치의 종류 ( light / gas 만 사용가능) | true | |
+| device_id | 스위치의 id (숫자) | true | |
+| interval | 스위치의 현재 상태를 모니터링하는 시간 (단위 millisecond) | false | 1000 |
+| manufacturer | 제조사명 | false | DefaultManufacturer |
+| model | 모델명 | false | DefaultModel |
+| serialnumber | 시리얼넘버 | false | DefaultSerialnumber |
+
+### Commax 서버 IP 와 우리집 IP 를 알아내는 법
+
+우선 http://www.ruvie.co.kr 사이트에 접속을 합니다.
+
+![Alt text](guide/guide_1.png?raw=true)
+
+사이트에 접속을 하셨으면
+
+상단의 홈스토리(HOME STORY) 메뉴에 들어가셔서 왼쪽에 홈제어 메뉴를 클릭합니다.
+
+![Alt text](guide/guide_2.png?raw=true)
+
+상단 브라우저에 보이는 주소가 commax_ip 입니다.
+
+위의 화면에서 크롬에서 소스보기를 합니다. 
+
+![Alt text](guide/guide_3.png?raw=true)
+
+소스중에 하일라이트 된 부분을 보시면 보이는 주소가 home_ip 입니다.
---
+++ index.js
@@ -0,0 +1,130 @@
+'use strict'
+
+let timer = require('timer')
+let request = require('request-promise')
+let isPolling = false
+
+let Service, Characteristic
+
+module.exports = (homebridge) => {
+    Service = homebridge.hap.Service
+    Characteristic = homebridge.hap.Characteristic
+    homebridge.registerAccessory('homebridge-commax-switch', 'CommaxSwitch', CommaxSwitchAccessory)
+}
+
+class CommaxSwitchAccessory {
+    constructor(log, config) {
+
+        this.log = log
+        this.config = config
+        this.commax_ip = config.commax_ip
+        this.home_ip = config.home_ip
+        this.device = config.device
+        this.device_id = config.device_id
+        this.interval = config.interval || 1000
+        this.manufacturer = config.manufacturer || "DefaultManufacturer"
+        this.model = config.model || "DefaultModel"
+        this.serialnumber = config.serialnumber || "DefaultSerialnumber"
+        this.service = new Service.Switch(this.config.name)
+        var that = this
+
+        // 주기적으로 스위치의 현재 상태 값을 가져옴
+        function check() {
+            // 주기적으로 가져오는 상태 값으로 스위치의 상태를 변경하지 않기 위해 isPolling boolean 을 사용함
+            // 스위치를 On 또는 Off 할때 주기적으로 가져오는 상태 값이 동기화가 안되어 엉뚱하게 동작하는 것을 방지함
+            isPolling = true
+            const state_options = {
+                uri: `http://${that.commax_ip}/center/state_device.php`,
+                qs: {
+                    dev_name: that.device,
+                    sel_no: that.device_id,
+                    h_ip: that.home_ip
+                }
+            }
+            request(state_options)
+                .then(
+                    (html) => {
+                        // console.log(html)
+                        if (html.includes('On') || html.includes('Open')) {
+                            isPolling = true
+                            that.service.getCharacteristic(Characteristic.On)
+                                .setValue(1)
+                        } else {
+                            isPolling = true
+                            that.service.getCharacteristic(Characteristic.On)
+                                .setValue(0)
+                        }
+                    }
+                )
+                .catch(
+                    (err) => console.error(err)
+                )
+            timer.timer(that.interval, check)
+        }
+        check()
+    }
+
+    getServices() {
+        const informationService = new Service.AccessoryInformation()
+            .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+            .setCharacteristic(Characteristic.Model, this.model)
+            .setCharacteristic(Characteristic.SerialNumber, this.serialnumber)            
+
+        this.service.getCharacteristic(Characteristic.On)
+            .on('get', this.getOnCharacteristicHandler.bind(this))
+            .on('set', this.setOnCharacteristicHandler.bind(this))
+
+        return [informationService, this.service]
+    }
+
+    setOnCharacteristicHandler(value, callback) {
+        this.isOn = value
+        if (value == true) {
+            // 주기적으로 가져온 상태 값이 아닌 악세서리 버튼을 눌렀을때 장비로 명령을 보냄
+            if (isPolling == false) {
+                const on_options = {
+                    uri: `http://${this.commax_ip}/center/${this.device}_action_device.php`,
+                    qs: {
+                        dev_name: this.device,
+                        sel_no: this.device_id,
+                        h_ip: this.home_ip,
+                        action: "ON"
+                    }
+                }
+                request(on_options)
+                    .then(
+                        // (html) => console.log(html)
+                    )
+                    .catch(
+                        (err) => console.error(err)
+                    )
+            }
+        } else {
+            // 주기적으로 가져온 상태 값이 아닌 악세서리 버튼을 눌렀을때 장비로 명령을 보냄
+            if (isPolling == false) {
+                const off_options = {
+                    uri: `http://${this.commax_ip}/center/${this.device}_action_device.php`,
+                    qs: {
+                        dev_name: this.device,
+                        sel_no: this.device_id,
+                        h_ip: this.home_ip,
+                        action: "OFF"
+                    }
+                }
+                request(off_options)
+                    .then(
+                        // (html) => console.log(html)
+                    )
+                    .catch(
+                        (err) => console.error(err)
+                    )
+            }
+        }
+        isPolling = false
+        callback(null)
+    }
+
+    getOnCharacteristicHandler(callback) {
+        callback(null, this.isOn)
+    }
+}
